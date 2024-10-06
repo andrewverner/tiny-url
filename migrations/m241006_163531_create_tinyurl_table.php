dc5ba5953dcfadbb0b105eee3c3dd4b399b6d292
Denis Khodakovskiy <khodakovskiy.d@it-webdevelopment.com>
2024-10-06 19:42:39 +0300

    Tiny URL migration

---
+++ migrations/m241006_163531_create_tinyurl_table.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%tiny_url}}`.
+ */
+class m241006_163531_create_tiny_url_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp(): void
+    {
+        $this->createTable(
+            table: '{{%tiny_url}}',
+            columns: [
+                'id' => $this->primaryKey(),
+                'original_url' => $this->string()->notNull(),
+                'hash' => $this->string(length: 5)->notNull(),
+                'created_at' => $this->dateTime()->notNull(),
+                'expired_at' => $this->dateTime()->notNull(),
+            ],
+            options: 'charset=utf8',
+        );
+        $this->createIndex(name: 'tiny_url_hash_idx', table: '{{%tiny_url}}', columns: 'hash');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown(): void
+    {
+        $this->dropIndex(name: 'tiny_url_hash_idx', table: '{{%tiny_url}}');
+        $this->dropTable(table: '{{%tiny_url}}');
+    }
+}
